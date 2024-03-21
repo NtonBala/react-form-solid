2eeaf92360d8bc85a638a2adf2c545aecbb47d6a
Anton Balashov <balashov.anton.1983@gmail.com>
2024-03-21 16:12:11 +0200

    Create SignupForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+const submitCredentials = (username: string, password: string) => {
+  return new Promise((resolve): void => {
+    setTimeout(() => {
+      resolve(null);
+      console.log(username, password);
+    }, 500);
+  });
+};
 
 const SignupForm = () => {
-  return <div>Sign up Form</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    loading: false,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setFormData((prevState) => ({ ...prevState, loading: true }));
+
+    submitCredentials(formData.username, formData.password).then(() => {
+      setFormData((prevState) => ({ ...prevState, loading: false }));
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        placeholder="Enter username"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        placeholder="Enter password"
+        onChange={handleChange}
+      />
+      <input type="submit" value="Submit" disabled={formData.loading} />
+    </form>
+  );
 };
 
 function App() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <SignupForm />
     </div>
   );
